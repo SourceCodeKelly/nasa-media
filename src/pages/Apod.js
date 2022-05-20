34db3aea1b41c61f0fb764e4a0030cdd040cbc74
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-05-20 09:13:30 -0400

    Added title to home

--- src/pages/Apod.js
+++ src/pages/Apod.js
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 export default function Apod() {
   return (
     <div className='Apod'>
-      <div className='Apod-btn'>
-      <Link to={'/nasaapod'} className='Apod-view'>See Into The Stars!</Link>
+      <div className='Apod-container'>
+        <h1>Welcome to NASA Pic of the Day</h1>
+        <div className='Apod-btn'>
+          <Link to={'/nasaapod'} className='Apod-view'>See Into The Stars!</Link>
+        </div>
       </div>
     </div>
   )
